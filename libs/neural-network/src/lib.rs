ea5cec66470bbc7442427926c8530a7897ca6f95
ArtamonovDen <brightcolors46@gmail.com>
2024-10-13 22:24:05 +0200

    feat: add them brain

--- libs/neural-network/src/lib.rs
+++ libs/neural-network/src/lib.rs
@@ -1,4 +1,5 @@
 use rand::{Rng, RngCore};
+use std::iter::once;
 
 #[derive(Debug)]
 pub struct Network {
@@ -13,6 +14,31 @@ pub struct LayerTopology {
 impl Network {
     fn new(layers: Vec<Layer>) -> Self {
         Self { layers }
+    }
+
+    pub fn from_weights(layers: &[LayerTopology], weights: impl IntoIterator<Item = f32>) -> Self {
+        assert!(layers.len() > 1);
+
+        let mut weights = weights.into_iter();
+
+        let layers = layers
+            .windows(2)
+            .map(|layers| Layer::from_weights(layers[0].neurons, layers[1].neurons, &mut weights))
+            .collect();
+
+        if weights.next().is_some() {
+            panic!("got too many weights");
+        }
+
+        Self { layers }
+    }
+
+    pub fn weights(&self) -> impl Iterator<Item = f32> + '_ {
+        self.layers
+            .iter()
+            .flat_map(|layer| layer.neurons.iter())
+            .flat_map(|neuron| once(&neuron.bias).chain(&neuron.weights))
+            .copied()
     }
 
     pub fn random(rng: &mut dyn RngCore, layers: &[LayerTopology]) -> Self {
@@ -47,6 +73,18 @@ impl Layer {
             .collect()
     }
 
+    fn from_weights(
+        input_size: usize,
+        output_size: usize,
+        weights: &mut dyn Iterator<Item = f32>,
+    ) -> Self {
+        let neurons = (0..output_size)
+            .map(|_| Neuron::from_weights(input_size, weights))
+            .collect();
+
+        Self { neurons }
+    }
+
     fn random(rng: &mut dyn RngCore, input_size: usize, output_size: usize) -> Self {
         // as we do not need neurons on the first layer, we init them only for the next step
 
@@ -76,6 +114,15 @@ impl Neuron {
 
         // ReLU and bias
         (output + self.bias).max(0.0)
+    }
+    fn from_weights(input_size: usize, weights: &mut dyn Iterator<Item = f32>) -> Self {
+        let bias = weights.next().expect("got not enough weights");
+
+        let weights = (0..input_size)
+            .map(|_| weights.next().expect("got not enough weights"))
+            .collect();
+
+        Self { bias, weights }
     }
 
     fn random(rng: &mut dyn RngCore, output_size: usize) -> Self {
@@ -119,5 +166,41 @@ mod tests {
             neuron.propagate(&[0.5, 1.0]),
             (-0.3 * 0.5) + (0.8 * 1.0) + 0.5
         )
+    }
+
+    #[test]
+    fn weights() {
+        let network = Network {
+            layers: vec![
+                Layer {
+                    neurons: vec![Neuron {
+                        bias: 0.1,
+                        weights: vec![0.2, 0.3, 0.4],
+                    }],
+                },
+                Layer {
+                    neurons: vec![Neuron {
+                        bias: 0.5,
+                        weights: vec![0.6, 0.7, 0.8],
+                    }],
+                },
+            ],
+        };
+
+        let actual: Vec<_> = network.weights().collect();
+        let expected = vec![0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8];
+
+        assert_relative_eq!(actual.as_slice(), expected.as_slice());
+    }
+
+    #[test]
+    fn from_weights() {
+        let layers = &[LayerTopology { neurons: 3 }, LayerTopology { neurons: 2 }];
+
+        let weights = vec![0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8];
+        let network = Network::from_weights(layers, weights.clone());
+        let actual: Vec<_> = network.weights().collect();
+
+        assert_relative_eq!(actual.as_slice(), weights.as_slice());
     }
 }
--- libs/simulation/src/lib.rs
+++ libs/simulation/src/lib.rs
@@ -1,18 +1,25 @@
 mod animal;
+mod animal_individual;
+mod brain;
 mod eye;
 mod food;
 mod world;
 
-pub use self::{animal::*, eye::*, food::*, world::*};
+pub use self::{animal::*, animal_individual::*, brain::*, eye::*, food::*, world::*};
 
+use lib_genetic_algorithm as ga;
 use lib_neural_network as nn;
 use nalgebra as na;
 use rand::{Rng, RngCore};
 
 use std::f32::consts::FRAC_PI_2;
 
+const GENERATION_LENGTH: usize = 2500;
+
 pub struct Simulation {
     world: World,
+    ga: ga::GeneticAlgorithm<ga::RouletteWheelSelection>,
+    age: usize,
 }
 
 const SPEED_MIN: f32 = 0.001;
@@ -22,9 +29,24 @@ const ROTATION_ACCEL: f32 = FRAC_PI_2;
 
 impl Simulation {
     pub fn random(rng: &mut dyn RngCore) -> Self {
-        Self {
-            world: World::random(rng),
-        }
+        let world = World::random(rng);
+
+        let ga = ga::GeneticAlgorithm::new(
+            ga::RouletteWheelSelection,
+            ga::UniformCrossover,
+            ga::GaussianMutation::new(0.01, 0.3),
+            // ---------------------- ^--^ -^-^
+            // | Chosen with a bit of experimentation.
+            // |
+            // | Higher values can make the simulation more chaotic,
+            // | which - a bit counterintuitively - might allow for
+            // | it to discover *better* solutions; but the trade-off
+            // | is that higher values might also cause current, good
+            // | enough solutions to be discarded.
+            // ---
+        );
+
+        Self { world, ga, age: 0 }
     }
 
     pub fn world(&self) -> &World {
@@ -46,6 +68,7 @@ impl Simulation {
                 let distance = na::distance(&animal.position, &food.position);
 
                 if distance <= 0.01 {
+                    animal.satiation += 1;
                     food.position = rng.gen();
                 }
             }
@@ -59,7 +82,7 @@ impl Simulation {
                     .eye
                     .process_vision(animal.position, animal.rotation, &self.world.foods);
 
-            let response = animal.brain.propagate(vision);
+            let response = animal.brain.nn.propagate(vision);
             let speed = response[0].clamp(-SPEED_ACCEL, SPEED_ACCEL);
             let rotation = response[1].clamp(-ROTATION_ACCEL, ROTATION_ACCEL);
             animal.speed = (animal.speed + speed).clamp(SPEED_MIN, SPEED_MAX);
@@ -71,5 +94,37 @@ impl Simulation {
         self.process_collisions(rng);
         self.process_brains();
         self.process_movements();
+        self.age += 1;
+
+        if self.age > GENERATION_LENGTH {
+            self.evolve(rng);
+        }
+    }
+    fn evolve(&mut self, rng: &mut dyn RngCore) {
+        self.age = 0;
+
+        // Step 1: Prepare birdies to be sent into the genetic algorithm
+        let current_population: Vec<_> = self
+            .world
+            .animals
+            .iter()
+            .map(AnimalIndividual::from_animal)
+            .collect();
+        // Evolves this `Vec<AnimalIndividual>`
+        let evolved_population = self.ga.evolve(rng, &current_population);
+
+        // Transforms `Vec<AnimalIndividual>` back into `Vec<Animal>`
+        self.world.animals = evolved_population
+            .into_iter()
+            .map(|individual| individual.into_animal(rng))
+            .collect();
+
+        // Step 4: Restart foods
+        //
+        // (this is not strictly necessary, but it allows to easily spot
+        // when the evolution happens - so it's more of a UI thing.)
+        for food in &mut self.world.foods {
+            food.position = rng.gen();
+        }
     }
 }
